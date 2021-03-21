86d830299650865e6c39782a241c383223856bd0
mikeBart0x <mikeBart0x@gmail.com>
2021-03-21 12:00:32 +0100

    initial commit

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,27 @@
+const CoinSwapFactory = artifacts.require("CoinSwapFactory.sol");
+const CalHash = artifacts.require("CalHash.sol");
+const TestToken1 = artifacts.require("TestToken1.sol");
+const TestToken2 = artifacts.require("TestToken2.sol");
+
+
+module.exports = async function (deployer, network, addresses ) {
+  await deployer.deploy(CoinSwapFactory, addresses[0]);
+  const factory = await CoinSwapFactory.deployed();
+
+  let token1Adress, token2Adress;
+  if (network == 'testnet' || 'bsc') {
+    token1Adress = '';
+    token2Adress = '';
+  } else {
+    await deployer.deploy(TestToken1);
+    await deployer.deploy(TestToken2);
+    const token1 = await TestToken1.deploy();
+    const token2 = await TestToken2.deploy();
+    token1Adress = token1.address;
+    token2Adress = token2.address;
+
+  }
+  await factory.createPair(token1Adress, token2Adress);
+  await deployer.deploy(CalHash);
+
+};

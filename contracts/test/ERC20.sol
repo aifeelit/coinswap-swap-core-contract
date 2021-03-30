b313b47c2be856416d12e5c93fd5c1015b188bd5
aifeelit <ifeelit416@gmail.com>
2021-03-31 01:24:30 +0200

    this commit change ERC20 on BEP20

--- contracts/test/ERC20.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity =0.5.16;
-
-import '../CoinSwapERC20.sol';
-
-contract ERC20 is CoinSwapERC20 {
-    constructor(uint _totalSupply) public {
-        _mint(msg.sender, _totalSupply);
-    }
-}
